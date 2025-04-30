50777df87c865885edd78b589cf559a29bce8463
Ivan Robi Septian <167876665+Ivan-R-Septian@users.noreply.github.com>
2025-04-30 16:53:54 +0700

    Update transform.js

--- api/transform.js
+++ api/transform.js
@@ -1,17 +1,19 @@
 export default async function handler(req, res) {
   const webhookUrl = "https://generative.3dolphins.ai:9443/dolphin/apiv1/graph/workflow/8d69eb3022b76f955740c336a18f66a4/WF/node-1744633504830/webhook";
 
+  const authHeader = req.headers['authorization'] || ''; // Ambil header, kalau nggak ada tetap kirim kosong
+
   try {
     const response = await fetch(webhookUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": authHeader
       },
       body: JSON.stringify(req.body),
     });
 
     const result = await response.json();
-
     const rawData = result?.data?.value;
 
     const transformed = {
